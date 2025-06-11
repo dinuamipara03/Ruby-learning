7aa8a6a33f4e2447ceda9ce5b4e4f76468a65e75
Divyarajsinh <divyaraj.sinh@team8network.com>
2025-06-11 18:30:06 +0530

    Add example demonstrating Queue operations in Ruby

---
+++ Day3/queue.rb
@@ -0,0 +1,23 @@
+q1=Queue.new
+q1.enq(1)
+puts q1.length
+
+
+#Ruby program for push()(<<) pop() function in Queue
+
+#Create a new QUEUE q1
+q1 = Queue.new
+
+#push 5
+     q1.push(5)
+
+#push 6
+         q1.push(6)
+
+#Prints the top - most element and also pops it
+             puts q1.pop
+
+                 puts q1.pop
+
+push=<<=enq
+pop=deq
