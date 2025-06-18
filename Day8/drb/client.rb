13699bd81cd05f3281c663b5fba48ab6515cdd73
Dinaxi AMIPARA <dinuamipara611@gmail.com>
2025-06-18 18:13:10 +0530

    Add examples for DRb client and server implementation in Ruby

---
+++ Day8/drb/client.rb
@@ -0,0 +1,17 @@
+require 'drb/drb'
+
+# The URI to connect to
+SERVER_URI="druby://localhost:8787"
+
+# Start a local DRbServer to handle callbacks.
+#
+# Not necessary for this small example, but will be required
+# as soon as we pass a non-marshallable object as an argument
+# to a dRuby call.
+#
+# Note: this must be called at least once per process to take any effect.
+# This is particularly important if your application forks.
+DRb.start_service
+
+timeserver = DRbObject.new_with_uri(SERVER_URI)
+puts timeserver.get_current_time
